4726b8d5cb8be810e22c577511f903c1a2c94173
Enbao <ecao25@sjs.org>
2025-02-10 16:18:32 -0600

    why the hell aren't these imports working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
-import ProjectsList from '../components/projects';
-import Pokemon from '../components/pokemon';
 import Header from '../components/header';
+import Pokemon from '../components/pokemon';
 import PostsList from '../components/postslist';
+import ProjectsList from '../components/projects';
 import { posts, getRecentPosts } from '../data/posts';
 
 export default function HomePage() {

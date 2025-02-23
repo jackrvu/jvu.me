32d492814ff37003c36349d500c463701fd94636
Enbao <ecao25@sjs.org>
2025-02-23 17:35:41 -0600

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
       <header className="pl-0 pt-4 pb-4 mb-4 border border-black border-t-0 border-l-0 border-r-0">
         <p className="text-base lg:text-lg font-semibold">enbao</p>
-        <p className="text-xs lg:text-sm">gradient stepper.</p>
+        <p className="text-xs lg:text-sm">gradient stepper. generalist.</p>
       </header>
     );
   }
